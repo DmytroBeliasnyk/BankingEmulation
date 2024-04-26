5a299bd4a91670627e68d635c8021648ea234190
DmytroBeliasnyk <beliasnykdmytro@gmail.com>
2024-04-26 18:45:58 +0200

    add jwt authentication

--- src/main/java/org/emuba/bankingemulation/configs/security/CustomUserDetailsService.java
+++ src/main/java/org/emuba/bankingemulation/configs/security/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
-package org.emuba.bankingemulation.services.impl;
+package org.emuba.bankingemulation.configs.security;
 
 import org.emuba.bankingemulation.models.CustomClient;
+import org.emuba.bankingemulation.services.impl.ClientServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -12,19 +14,20 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class UserDetailServiceImpl implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
+
     private final ClientServiceImpl clientService;
 
-    public UserDetailServiceImpl(ClientServiceImpl clientService) {
+    @Autowired
+    public CustomUserDetailsService(ClientServiceImpl clientService) {
         this.clientService = clientService;
     }
 
-
     @Override
-    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-        CustomClient client = clientService.findClientByLogin(login);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        CustomClient client = clientService.findClientByLogin(username);
         if (client == null)
-            throw new UsernameNotFoundException("Login: " + login);
+            throw new UsernameNotFoundException("Username not found");
 
         List<GrantedAuthority> roles = List.of(
                 new SimpleGrantedAuthority(client.getRole().toString()));
@@ -32,3 +35,4 @@ public class UserDetailServiceImpl implements UserDetailsService {
         return new User(client.getLogin(), client.getPassword(), roles);
     }
 }
+
---
+++ src/main/java/org/emuba/bankingemulation/dto/LoginDTO.java
@@ -0,0 +1,9 @@
+package org.emuba.bankingemulation.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    private String username;
+    private String password;
+}
