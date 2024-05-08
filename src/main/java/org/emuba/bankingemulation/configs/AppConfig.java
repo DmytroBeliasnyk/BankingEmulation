d6afe8c0c86a942262a7fac60254a67d66250d7d
DmytroBeliasnyk <beliasnykdmytro@gmail.com>
2024-05-08 13:38:41 +0200

    add cors configuration

--- src/main/java/org/emuba/bankingemulation/configs/AppConfig.java
+++ src/main/java/org/emuba/bankingemulation/configs/AppConfig.java
@@ -9,12 +9,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.net.URISyntaxException;
 import java.util.Properties;
 
 @Configuration
-public class AppConfig {
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
     @Bean
     public CommandLineRunner commandLineRunner(final ClientServiceImpl clientService,
                                                final PasswordEncoder encoder) {
@@ -45,6 +49,11 @@ public class AppConfig {
         props.put("mail.debug", "true");
 
         return mailSender;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
     @Bean
