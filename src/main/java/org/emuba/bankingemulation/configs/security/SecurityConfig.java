3c88ca69260f2bbe6e786d2bbd331a167bc0d8ef
DmytroBeliasnyk <beliasnykdmytro@gmail.com>
2024-05-08 13:42:17 +0200

    add cors configuration

--- src/main/java/org/emuba/bankingemulation/configs/security/SecurityConfig.java
+++ src/main/java/org/emuba/bankingemulation/configs/security/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -71,10 +72,14 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+
         configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
 }
