25950985b95ae4ee736c37b253a071a77004a68c
DmytroBeliasnyk <beliasnykdmytro@gmail.com>
2024-05-14 11:49:28 +0200

    add CSP configuration

--- src/main/java/org/emuba/bankingemulation/configs/security/SecurityConfig.java
+++ src/main/java/org/emuba/bankingemulation/configs/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -37,6 +38,10 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
+                .headers(configurer->configurer
+                        .contentSecurityPolicy(contentSecurityPolicyConfig ->
+                                contentSecurityPolicyConfig.policyDirectives(
+                                        "default-src *;")))
                 .exceptionHandling(handling -> handling
                         .authenticationEntryPoint(authEntryPoint))
                 .sessionManagement(management -> management
