0293cae93ddd4388405b43fe50e2b73e9195e4f7
DmytroBeliasnyk <beliasnykdmytro@gmail.com>
2024-05-14 12:13:55 +0200

    add CSP configuration

--- src/main/java/org/emuba/bankingemulation/configs/security/SecurityConfig.java
+++ src/main/java/org/emuba/bankingemulation/configs/security/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,7 +18,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -38,10 +36,10 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
-                .headers(configurer->configurer
+                .headers(configurer -> configurer
                         .contentSecurityPolicy(contentSecurityPolicyConfig ->
                                 contentSecurityPolicyConfig.policyDirectives(
-                                        "default-src 'self';")))
+                                        "script-src *;")))
                 .exceptionHandling(handling -> handling
                         .authenticationEntryPoint(authEntryPoint))
                 .sessionManagement(management -> management
