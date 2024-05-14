8f387d41a7b63fe2c328e72473762e1387997838
DmytroBeliasnyk <beliasnykdmytro@gmail.com>
2024-05-14 16:35:49 +0200

    с

--- src/main/java/org/emuba/bankingemulation/configs/security/SecurityConfig.java
+++ src/main/java/org/emuba/bankingemulation/configs/security/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -36,10 +35,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
-                .headers(headers -> headers
-                        .addHeaderWriter(new StaticHeadersWriter("Content-Security-Policy",
-                                "default-src *")))
+                .cors(cors -> cors
+                        .configurationSource(corsConfigurationSource()))
                 .exceptionHandling(handling -> handling
                         .authenticationEntryPoint(authEntryPoint))
                 .sessionManagement(management -> management
