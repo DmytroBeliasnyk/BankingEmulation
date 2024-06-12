c669f75e8b722e70b87a7adf5c4591f88d6e07f4
DmytroBeliasnyk <beliasnykdmytro@gmail.com>
2024-06-12 14:47:21 +0200

    update refactor AdminController and ClientController

--- src/main/java/org/emuba/bankingemulation/configs/AppConfig.java
+++ src/main/java/org/emuba/bankingemulation/configs/AppConfig.java
@@ -1,10 +1,8 @@
 package org.emuba.bankingemulation.configs;
 
-import org.emuba.bankingemulation.enums.TypeCurrency;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.emuba.bankingemulation.enums.UserRole;
-import org.emuba.bankingemulation.models.CustomClient;
 import org.emuba.bankingemulation.services.impl.ClientServiceImpl;
-import org.emuba.bankingemulation.services.impl.HistoryServiceImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -14,8 +12,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 @PropertySource("classpath:email_config.txt")
@@ -28,8 +25,7 @@ public class AppConfig {
 
     @Bean
     public CommandLineRunner commandLineRunner(final ClientServiceImpl clientService,
-                                               final PasswordEncoder encoder,
-                                               final HistoryServiceImpl historyService) {
+                                               final PasswordEncoder encoder) {
         return new CommandLineRunner() {
             @Override
             public void run(String... args) throws Exception {
@@ -37,14 +33,6 @@ public class AppConfig {
                         "admin", encoder.encode("123admin"), UserRole.ADMIN);
                 clientService.addClient("Dmytro", "Beliasnyk", "dimabelasnik6@gmail.com",
                         "user", encoder.encode("123user"), UserRole.USER);
-
-                CustomClient client1 = clientService.findClientByLogin("user");
-                historyService.saveTransaction(client1, "123123", TypeCurrency.USD,
-                        client1, "123123", TypeCurrency.USD, LocalDate.of(2024, 5, 10), BigDecimal.TEN);
-                historyService.saveTransaction(client1, "123123", TypeCurrency.USD,
-                        client1, "123123", TypeCurrency.USD, LocalDate.of(2024, 5, 15), BigDecimal.TEN);
-                historyService.saveTransaction(client1, "123123", TypeCurrency.USD,
-                        client1, "123123", TypeCurrency.USD, LocalDate.now(), BigDecimal.TEN);
             }
         };
     }
@@ -67,18 +55,18 @@ public class AppConfig {
         return mailSender;
     }
 
-//    @Bean
-//    public BasicDataSource dataSource() throws URISyntaxException {
-//        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-//        String username = System.getenv("JDBC_DATABASE_USERNAME");
-//        String password = System.getenv("JDBC_DATABASE_PASSWORD");
-//
-//        BasicDataSource basicDataSource = new BasicDataSource();
-//        basicDataSource.setUrl(dbUrl);
-//        basicDataSource.setUsername(username);
-//        basicDataSource.setPassword(password);
-//
-//        return basicDataSource;
-//    }
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 }
 
